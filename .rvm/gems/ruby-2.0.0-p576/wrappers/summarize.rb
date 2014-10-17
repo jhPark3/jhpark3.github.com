cc8f7c0eca97f063795577e672ac6088b287d8b9
joongho <kikiki1011@hanmail.net>
2014-10-17 11:33:16 +0900

    Initialize blog

---
+++ .rvm/gems/ruby-2.0.0-p576/wrappers/summarize.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if
+  [[ -s "/home/joonghopark/.rvm/gems/ruby-2.0.0-p576/environment" ]]
+then
+  source "/home/joonghopark/.rvm/gems/ruby-2.0.0-p576/environment"
+  exec ruby summarize.rb "$@"
+else
+  echo "ERROR: Missing RVM environment file: '/home/joonghopark/.rvm/gems/ruby-2.0.0-p576/environment'" >&2
+  exit 1
+fi
